6f87341f48ba6065ca651973260bcff5fcc30627
zen <designerzen@gmail.com>
2026-05-27 16:07:34 +0100

    feat: playwright + vittest

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
   plugins: [react()],
   worker: {
     format: "es"
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
+    include: ["tests/**/*.test.{ts,tsx}"],
+    exclude: ["tests/e2e/**"]
   }
 });
